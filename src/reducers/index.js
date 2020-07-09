f032737b1bd32c02969f4147fb4ce43343046b49
kovalchuk-mykhailo <yakobiant@gmail.com>
2020-07-09 21:05:03 +0300

    Updated a main reducer.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import { currenciesReducer } from "./currencies";
 import { commandInputReducer } from "./commandInput";
+import { expensesReducer } from "./expenses";
 
 const allReducers = combineReducers({
   currencies: currenciesReducer,
   commandInput: commandInputReducer,
+  expenses: expensesReducer,
 });
 
 export default allReducers;
